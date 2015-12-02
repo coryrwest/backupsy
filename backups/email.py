68d57ea2275061052622e5a27f5019fea98ec976
Cory Westropp <cory.r.west@gmail.com>
2015-12-02 23:27:54 +0000

    Initial Commit

---
+++ backups/email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from optparse import OptionParser
+import subprocess
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-u", "--username", action="store", type="string",
+                  help="username for login", dest="username")
+    parser.add_option("-p", "--password", action="store", type="string",
+                  help="password for login", dest="password")
+    parser.add_option("-g", "--gmail",
+                  action="store_true", dest="gmail", default=False,
+                  help="is this a gmail account?")
+    (options, args) = parser.parse_args()
+    if(options.gmail):
+        subprocess.call(["gmvault", "-h"])
+    else:
+	print "regular email"
+
+# do work here
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="Backupsy-Backups",
+    version="1.0",
+    install_requires=[
+        "gmvault"
+    ]
+)
